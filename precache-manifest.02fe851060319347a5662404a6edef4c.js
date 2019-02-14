e72d5aa13392a0660b8438293879c17dea88d1fd
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-02-14 18:50:34 +0000

    Deploy carsimoes/myreads to github.com/carsimoes/myreads.git:gh-pages

---
+++ precache-manifest.02fe851060319347a5662404a6edef4c.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = [
+  {
+    "revision": "301c29a76a4b3a69b15a",
+    "url": "/static/css/main.68f13593.chunk.css"
+  },
+  {
+    "revision": "301c29a76a4b3a69b15a",
+    "url": "/static/js/main.301c29a7.chunk.js"
+  },
+  {
+    "revision": "ba4ee9f3e09de5cbbc89",
+    "url": "/static/css/1.5dcbcee6.chunk.css"
+  },
+  {
+    "revision": "ba4ee9f3e09de5cbbc89",
+    "url": "/static/js/1.ba4ee9f3.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "bb9363b86b02777d23523851826414e0",
+    "url": "/static/media/add.bb9363b8.svg"
+  },
+  {
+    "revision": "3687aa988bec28f17ae39153db1646c3",
+    "url": "/static/media/arrow-back.3687aa98.svg"
+  },
+  {
+    "revision": "78c6f53b2342643639fe1cbaf171bf47",
+    "url": "/static/media/arrow-drop-down.78c6f53b.svg"
+  },
+  {
+    "revision": "10d0705be2a7e9054e43cf1bd027be5c",
+    "url": "/index.html"
+  }
+];
